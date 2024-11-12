f5202d596c4c60c8a3eeb9fb03868af490303d22
Natanael Arndt <arndtn@gmail.com>
2024-11-12 23:24:43 +0100

    Also export the fam: namespace

--- simpsons.py
+++ simpsons.py
@@ -2,6 +2,7 @@ from rdflib import Graph, Namespace
 import importlib.resources
 
 SIM = Namespace("https://simpsons.example.org/")
+FAM = Namespace("https://vocab.example.org/family#")
 
 graph = Graph()
 with importlib.resources.path(__name__, "simpsons.ttl") as data_path:
