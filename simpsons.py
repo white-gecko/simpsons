8e66e23d07499c754b5d4b89ffa48a16d7efe4d0
Natanael Arndt <arndtn@gmail.com>
2024-11-13 18:26:00 +0100

    delay graph parsing

--- simpsons.py
+++ simpsons.py
@@ -4,6 +4,17 @@ import importlib.resources
 SIM = Namespace("https://simpsons.example.org/")
 FAM = Namespace("https://vocab.example.org/family#")
 
-graph = Graph()
-with importlib.resources.path(__name__, "simpsons.ttl") as data_path:
-    graph.parse(data_path, format="turtle")
+class _Simpsons():
+    _graph = None
+
+    @property
+    def graph(self):
+        print("run")
+        if self._graph is None:
+            self._graph = Graph()
+            with importlib.resources.path(__name__, "simpsons.ttl") as data_path:
+                self._graph.parse(data_path, format="turtle")
+        return self._graph
+
+_simpsons = _Simpsons()
+graph = getattr(_simpsons, "graph")
