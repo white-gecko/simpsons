c495b9a3fce9f9a33ab5c1b4df798eb56d7756ed
Natanael Arndt <arndtn@gmail.com>
2024-11-13 21:49:38 +0100

    Add example.py

---
+++ example.py
@@ -0,0 +1,5 @@
+from simpsons import simpsons, SIM, FAM
+
+print({p: o for _, p, o in simpsons.graph.triples((SIM.Homer, None, None))})
+print([spouse for _, _, spouse in simpsons.graph.triples((SIM.Homer, FAM.hasSpouse, None))])
+print(simpsons.graph.serialize(format="turtle"))
