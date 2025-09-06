2788c49ab5f11f9fc77e871b4a30ab1339bd87dd
Natanael Arndt <arndtn@gmail.com>
2025-09-06 22:45:07 +0200

    Move single file module to folder, make it work with python 3.9, more tests, and graph_file property.

--- simpsons_rdf/__init__.py
+++ simpsons_rdf/__init__.py
@@ -1,6 +1,8 @@
-from rdflib import Graph, Namespace
 import importlib.resources
 from functools import cache
+from pathlib import Path
+
+from rdflib import Graph, Namespace
 
 SIM = Namespace("https://simpsons.example.org/")
 FAM = Namespace("https://vocab.example.org/family#")
@@ -9,9 +11,13 @@ FAM = Namespace("https://vocab.example.org/family#")
 class Simpsons:
     @property
     @cache
-    def graph(self):
+    def graph(self) -> Graph:
+        return Graph(identifier=SIM[""]).parse(self.graph_file, format="turtle")
+
+    @property
+    def graph_file(self) -> Path:
         with importlib.resources.path(self.__module__, "simpsons.ttl") as data_path:
-            return Graph(identifier=SIM[""]).parse(data_path, format="turtle")
+            return data_path
 
 
 simpsons = Simpsons()
--- tests/test_simpsons.py
+++ tests/test_simpsons.py
@@ -1,9 +1,19 @@
-from simpsons_rdf import simpsons, SIM
 from rdflib import URIRef
 
+from simpsons_rdf import SIM, simpsons
 
-def test_simpsons():
+
+def test_simpsons_homer_exists():
+    """Check if any triple with Homer as subject exists."""
     assert len(list(simpsons.graph.triples((SIM.Homer, None, None)))) > 0
 
-def test_simpsons():
+
+def test_simpsons_graph_identifier():
+    """Check if the graph identifier is as expected."""
     assert simpsons.graph.identifier == URIRef("https://simpsons.example.org/")
+
+
+def test_simpsons_get_file():
+    """Check if the graph identifier is as expected."""
+    assert simpsons.graph_file.name == "simpsons.ttl"
+    assert simpsons.graph_file.exists()
